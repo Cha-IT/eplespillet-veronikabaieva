9fec7904ce8852475f683575a0c26d49c7d315a0
mariiyamm <mary2802sh@gmail.com>
2025-10-03 15:38:08 +0200

    spill med epler

--- js/fruktplukker.js
+++ js/fruktplukker.js
@@ -1,20 +1,59 @@
 // Først, opprett en knapp for å generere fruktene
 const button = document.createElement("button");
-button.innerHTML = "Generer frukt";
+button.innerHTML = "Start Game";
 document.body.appendChild(button);
+button.style.position= "absolute";
+button.style.top = "40%";
+button.style.left = "40%";
+button.style.fontSize = "50px"
 // Når knappen klikkes, generer en ny frukt
-button.addEventListener("click", nyFrukt)
+button.addEventListener("click", () => {
+    button.style.display = "none";
+    nyFrukt()
+});
 
-function nyFrukt() 
-{
+function spillSlutt(){
+    const gameOverbutton = document.createElement("button");
+    gameOverbutton.innerHTML = "Game Over";
+    document.body.appendChild(gameOverbutton);
+    gameOverbutton.style.position= "absolute";
+    gameOverbutton.style.top = "40%";
+    gameOverbutton.style.left = "40%";
+    gameOverbutton.style.fontSize = "50px"
+    gameOverbutton.addEventListener("click", () => {
+        gameOverbutton.style.display = "none";
+        nyFrukt()
+    })
+    
+}
+
+function nyFrukt() {
     const frukt = document.createElement("div");
     frukt.innerHTML = "🍎"; // Du kan endre dette til forskjellige frukt emojis
-    frukt.style.fontSize = "2em";
+    frukt.style.fontSize = "100px";
     frukt.style.position = "absolute";
-    frukt.style.left = Math.random() * window.innerWidth + 'px'; // Plasser frukten på en tilfeldig x-posisjon
-    frukt.style.top = Math.random() * window.innerHeight + 'px'; // Plasser frukten på en tilfeldig y-posisjon
+
+    let posX = Math.random() * (window.innerWidth - 50);
+    let posY = Math.random() * (window.innerHeight - 700);
+
+    frukt.style.left = posX + 'px';
+    frukt.style.top = posY + 'px';
+
     document.body.appendChild(frukt);
- 
+
+    const speedY = 3;
+
+    function move (){
+        posY += speedY;
+        frukt.style.top = posY + 'px';
+        if (posY < window.innerHeight - 100){
+            requestAnimationFrame(move);
+        } else {
+            spillSlutt();
+        }
+    }
+    setTimeout(nyFrukt,2000);
+    setTimeout(move,3000);
     // Når frukten klikkes, fjern den fra skjermen
     frukt.addEventListener("click", fjernFrukt)
 }
@@ -26,3 +65,5 @@ function fjernFrukt(e)
     document.body.removeChild(e.target); 
     //e.target er det elementet som trigget hendelsen, det vil si elementet vi klikket på for å aktivere funksjonen.
 }
+
+
